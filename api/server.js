554ff5290c0baf07e49690db1d50851b19dcaba4
Sara Chein <sarahchein@gmail.com>
2025-02-19 13:26:20 -0500

    sorted

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ const taskRouter = require('./task/router')
 const server = express()
 
 server.use(express.json())
+
 server.use('/api/projects', projectRouter)
 server.use('/api/resources', resourceRouter)
 server.use('/api/tasks', taskRouter)
